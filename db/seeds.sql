ab6f83cfc5fa6a35c17811e73fa1fe842ced3162
mghezel <mahtab.ghezel@gmail.com>
2017-10-30 22:58:50 -0700

    submit needs to be worked on, should not return empty object

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, burger_time) VALUES ('Chicken burger', false, NOW());
+INSERT INTO burgers (burger_name, devoured, burger_time) VALUES ('Shrimp burger', false, NOW());
+INSERT INTO burgers (burger_name, devoured, burger_time) VALUES ('Bacon burger', false, NOW());
+INSERT INTO burgers (burger_name, devoured, burger_time) VALUES ('Veg burger', false, NOW());
